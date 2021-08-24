c5a7e0af504b21f22a16d144a2a5ace359ce0b21
phariven <harivenkatesh_polnati@optum.com>
2021-08-24 11:25:28 +0530

    Middlewares

--- index.js
+++ index.js
@@ -1,47 +1,68 @@
 const express = require('express')
+const druglist = require('./constants');
+const logger = require('./logger');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); // 
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
 
-
-const druglist = [
-    {
-      id: 'd1',
-      title: 'Crocin',
-      amount: 14.12,
-      isPopular: true,
-      desc: 'Crocin Advance is the best medicine for Joint, knee, muscles and other pains which provide fast and effective relief. It is suitable for adults as well as',
-    },
-    {
-      id: 'd2',
-      title: 'Zincovit',
-      amount: 4.12,
-      isPopular: false,
-      desc: 'It is a nutritional supplement that restores the amount of vitamins and minerals in your body and helps to improve your overall health.',
-    },
-    {
-      id: 'd3',
-      title: 'Lipitor',
-      amount: 194.28,
-      isPopular: false,
-      desc: 'LIPITOR is a prescription medicine that lowers cholesterol in the blood. It lowers the LDL-C ("bad" cholesterol) and triglycerides in your blood',
-    },
-    {
-      id: 'd4',
-      title: 'Sildenafil  ',
-      amount: 694.88,
-      isPopular: true,
-      desc: 'Sildenafil is used to treat  function problems (impotence or erectile dysfunction-ED). In combination with sildenafil works',
-    }
-  ];
+app.use(logger); // custom middleware
 
 app.get('/', (req, res) => {
-    res.send("Hello Express!");
+  //locahost:8000/1/vitd
+  const params = req.query;
+  console.log(params)
+  res.send(params);
 });
 
+//GET - drug all
 app.get('/api/drugs', (req, res) => {
-    res.send(druglist);
-})
+  res.send(druglist);
+});
 
 
-app.listen(8000, ()=> { console.log('Server is listeing on port 8000')});
+//READ get by id
+app.get('/api/drugs/:id', (req, res) => {
+
+  const {id} = req.params;
+
+  const drug = druglist.find(drug =>  drug.id === id);
+
+  if(!drug) // for better validation use Joi
+  {
+    res.status(404).send('This drug id is not available');
+  }
+
+  res.send(drug);
+});
+
+//Creation
+app.post('/api/drugs', (req,res) => {
+
+  const drugObj = req.body;
+  
+  const drug =  {
+    id: 'd' + (druglist.length+1) ,
+    title: drugObj.title,
+    amount: drugObj.amount,
+    isPopular: drugObj.isPopular,
+    desc: drugObj.desc
+  }
+  druglist.push(drug);
+  res.send(drug);
+});
+
+
+//PUT for update
+
+//DELETE for deletion
+
+// Above 2 are assignment fot you
+
+app.listen(8000, () => { console.log('Server is listeing on port 8000') });
